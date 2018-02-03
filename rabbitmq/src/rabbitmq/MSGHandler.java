414c465bca396f36cb9708665cb32ba30b058f25
silvia <silviasottile94@gmail.com>
2018-02-03 23:10:40 +0100

    Added POST request in MSG Handler Però c'è un problema quando provo a fare la post con i dati reali, mentre con dei dati fittizi va tutto a buon fine. Bisogna vedere perchè.

--- rabbitmq/src/rabbitmq/MSGHandler.java
+++ rabbitmq/src/rabbitmq/MSGHandler.java
@@ -4,6 +4,17 @@ import com.rabbitmq.client.*;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import Util.Log;
+import com.google.gson.Gson;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Il MSGHandler salva il log sul DB solo se l’oggetto è 
@@ -12,7 +23,6 @@ import java.util.regex.Pattern;
  * @author silvia
  */
 public class MSGHandler {
-    // TODO: vedere come alzare l'interfaccia REST
     private static final String EXCHANGE_NAME = "logs";
 
     public static void main(String[] argv) throws Exception {
@@ -43,29 +53,50 @@ public class MSGHandler {
                 String message = new String(body, "UTF-8");
                 // System.out.println(" [x] Received '" + message + "'");
                 // analyze string and check for particular message and IP address
+                String timestamp = null, machineID = null, msg = null;
+                Timestamp timestampSql = null;
                 if (message.contains("Error receiving object from") &
                         (message.contains("10.18.122.24") | 
                         message.contains("10.18.122.30"))) {
                     // extract timestamp, machine ID and body from message
                     Matcher machineIDMatcher = machineIDPattern.matcher(message);
                     if (machineIDMatcher.find()) {
-                        String machineID = machineIDMatcher.group();
+                        machineID = machineIDMatcher.group();
                         // strip from useless characters
                         machineID = machineID.replaceAll("WARN  ", "").replaceAll(" - ", "");
-                        System.out.println(machineID); // to be saved in db
+                        System.out.println(machineID);
                     }
                     Matcher timestampMatcher = timestampPattern.matcher(message);
                     if (timestampMatcher.find()) {
-                        String timestamp = timestampMatcher.group();
-                        System.out.println(timestamp); // to be saved in db
+                        timestamp = timestampMatcher.group();
+                        System.out.println(timestamp);
+                        // convert timestamp string to java.sql.Timestamp
+                        try {
+                            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+                            Date parsedDate = dateFormat.parse(timestamp);
+                            timestampSql = new java.sql.Timestamp(parsedDate.getTime());
+                        } catch (ParseException ex) {
+                            Logger.getLogger(MSGHandler.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        
                     }
                     Matcher msgMatcher = msgPattern.matcher(message);
                     if (msgMatcher.find()) {
-                        String msg = msgMatcher.group();
+                        msg = msgMatcher.group();
                         msg = msg.replaceAll(" - ", "");
-                        System.out.println(msg); // to be saved in db
+                        System.out.println(msg);
                     }
                 }
+                // Send log to controller via POST request
+                Client client = Client.create();
+                //Log log = new Log(timestampSql, machineID, msg);
+                Date date = new Date();
+                Log log = new Log(new java.sql.Timestamp(date.getTime()), "prova", "prova");
+                System.out.println("VOGLIO INVIARE: " + log.toString());
+                String string = new Gson().toJson(log);
+                WebResource webResourcePost = client.resource("http://localhost:8080/QuorumProject-war/gestione/log/post");
+                ClientResponse rispostaPost = webResourcePost.post(ClientResponse.class, string);
+                System.out.println("HO RICEVUTO: " + rispostaPost.getEntity(String.class));                
             }
         };
         channel.basicConsume(queueName, true, consumer);
