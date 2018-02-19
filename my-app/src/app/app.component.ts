c3ba1846a94198bc8a9a757d2a6fd6b03c338829
silvia <silviasottile@hotmail.it>
2018-02-19 19:24:24 +0100

    GUI finita! Molto spartana ma completa, forse manca un po' di stile

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -11,12 +11,10 @@ export class AppComponent {
   title = 'QuorumProject';
   private endpoint = 'http://localhost:8080/QuorumProject-war/gestione/log/';
   data: any = {};
-  resultTitle = '';
+  resultTitle = 'The results will be shown here...';
 
-  constructor(private _http: Http) {
-    // this.getAllLogs();
-    // this.getLastEvent();
-  }
+
+  constructor(private _http: Http) {  }
 
   public getAllLogs() {
     return this._http.get(this.endpoint + 'get')
@@ -34,6 +32,26 @@ export class AppComponent {
                  .subscribe(data => {
                         this.data = data;
                         this.resultTitle = 'Last event';
+                        console.log(this.data);
+                });
+  }
+
+  public getMachineID(id: string) {
+    return this._http.get(this.endpoint + 'get/' + id)
+                .map((res: Response) => res.json())
+                 .subscribe(data => {
+                        this.data = data;
+                        this.resultTitle = 'Logs from ' + id + ':';
+                        console.log(this.data);
+                });
+  }
+
+  public searchTimeInterval(begin: string, end: string) {
+    return this._http.get(this.endpoint + 'timestamp/' + begin + '/' + end)
+                .map((res: Response) => res.json())
+                .subscribe(data => {
+                        this.data = data;
+                        this.resultTitle = 'Logs from ' + begin + ' to ' + end + ':';
                         console.log(this.data);
                 });
   }
