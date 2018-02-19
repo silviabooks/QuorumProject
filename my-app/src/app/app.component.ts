b97f831c58cdcb6a34ad4ba346ed1d4b08e5b26e
silvia <silviasottile@hotmail.it>
2018-02-19 18:27:06 +0100

    [WIP] prime due GET finite, ora mancano le due richieste con i parametri

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -9,18 +9,31 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent {
   title = 'QuorumProject';
-  private endpoint = 'http://localhost:8080/QuorumProject-war/gestione/log';
+  private endpoint = 'http://localhost:8080/QuorumProject-war/gestione/log/';
   data: any = {};
+  resultTitle = '';
 
   constructor(private _http: Http) {
-    this.getAllLogs();
+    // this.getAllLogs();
+    // this.getLastEvent();
   }
 
-  private getAllLogs() {
-    return this._http.get('http://localhost:8080/QuorumProject-war/gestione/log/get')
+  public getAllLogs() {
+    return this._http.get(this.endpoint + 'get')
                 .map((res: Response) => res.json())
                  .subscribe(data => {
                         this.data = data;
+                        this.resultTitle = 'All logs';
+                        console.log(this.data);
+                });
+  }
+
+  public getLastEvent() {
+    return this._http.get(this.endpoint + 'getLast')
+                .map((res: Response) => res.json())
+                 .subscribe(data => {
+                        this.data = data;
+                        this.resultTitle = 'Last event';
                         console.log(this.data);
                 });
   }
